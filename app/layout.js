1965416ac3b4a1986ef71f8d5616f1645eff38a8
Ahad Ahmad <124980699+YoAhdi@users.noreply.github.com>
2024-08-21 11:27:22 -0700

    Add files via upload

---
+++ app/layout.js
@@ -0,0 +1,18 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "AI Rate My Professor",
+  description:
+    "A chat-based app that helps students find and rate professors using AI-driven semantic search.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,18 @@
+"use client";
+import { Box, Typography } from "@mui/material";
+import { useState, useEffect } from 'react';
+
+export default function Home() {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      width="100vw"
+      height="100vh"
+      alignItems="center"
+      justifyContent="center" m={0}
+    >
+      <Typography variant="h3">AI Rate My Professor</Typography>
+    </Box>
+  );
+}
