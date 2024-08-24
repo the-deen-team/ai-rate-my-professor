17eec4189453ae4f9802c2a042c89c38674af83f
YoAhdi <ahadahmad.work@gmail.com>
2024-08-24 15:05:23 -0700

    first part front end complete

--- app/page.js
+++ app/page.js
@@ -25,10 +25,31 @@ export default function Home() {
       'Content-Type:', 'application/json'
     },
     body: JSON.stringify([...messages, {role: "user", content: message}])
-  }).then(async(res))
+  }).then(async(res)=>{
+    const reader = res.body.getReader()
+    const decoder = new TextDecoder()
+
+    let result = ''
+    return reader.read().then(function processText({done, value}) {
+      if (done){
+        return result
+      }
+      const text = decoder.decode (value [] new Uint8Array(), {stream: true})
+      setMessages((messages)=>{
+        let lastMessage = messages[messages.length - 1]
+        let otherMessages = messages.slice(0, messages.length - 1)
+        return[
+          ...otherMessages,
+          {...lastMessage, content, lastMessage.content + text},
+
+        ]
+      })
+      return reader.read().then(processText)
+    })
+
+  })
+
 
 }
-  return(
-  
-  );
+  return<></>
 }
